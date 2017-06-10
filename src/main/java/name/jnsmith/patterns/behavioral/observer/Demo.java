d066ca6ee4064327b0c2bbb95b8e181b412838e4
jswizzy <smithjustinn@hotmail.com>
2017-06-10 01:45:32 -0400

    add observer pattern

---
+++ src/main/java/name/jnsmith/patterns/behavioral/observer/Demo.java
@@ -0,0 +1,14 @@
+package name.jnsmith.patterns.behavioral.observer;
+
+public class Demo {
+
+    public static void main(String args[]) {
+        Subject subject = new MessageStream();
+
+        PhoneClient phoneClient = new PhoneClient(subject);
+        TabletClient tabletClient = new TabletClient(subject);
+
+        phoneClient.addMessage("Here is a new message!");
+        tabletClient.addMessage("Another new message!");
+    }
+}
